41d4caa30e1725bef0dcf7c089610ee74bef890f
Jakob Gillich <jakob@gillich.me>
2022-04-09 22:09:11 +0200

    foo

--- main.cr
+++ main.cr
@@ -2,7 +2,6 @@ ch = Channel(Nil).new
 ["a", "b", "c", "d", "e", "f", "g"].map do |path|
   dir = Path[Dir.current, path]
   spawn do
-    puts dir.to_s
     p = Process.new("shards", args: ["install", "--frozen"], chdir: dir.to_s, input: Process::Redirect::Inherit, output: Process::Redirect::Inherit, error: Process::Redirect::Inherit)
     unless p.wait.normal_exit?
       exit 1
@@ -11,7 +10,6 @@ ch = Channel(Nil).new
     unless p.wait.normal_exit?
       exit 1
     end
-
     ch.send(nil)
   end
 end.each { ch.receive }
