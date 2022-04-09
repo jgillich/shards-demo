25a864432ebb2ec9fa2c03842c42056502e8e74a
Jakob Gillich <jakob@gillich.me>
2022-04-09 21:54:49 +0200

    foo

--- main.cr
+++ main.cr
@@ -1,6 +1,6 @@
 ["a", "b", "c"].each do |path|
   dir = Path[Dir.current, path]
   spawn do
-    Process.exec("shards", args: ["install"], chdir: dir.to_s)
+    Process.run("shards", args: ["install"], chdir: dir.to_s, input: Process::Redirect::Inherit, output: Process::Redirect::Inherit, error: Process::Redirect::Inherit,)
   end
 end
